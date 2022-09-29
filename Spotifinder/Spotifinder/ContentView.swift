832755912ed66e27131ec253de236690a59e24a8
Martin Lago <martinlago0901@gmail.com>
2022-09-29 16:24:48 -0300

    Add base iOS project

---
+++ Spotifinder/Spotifinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spotifinder
+//
+//  Created by Martín Lago on 29/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Spotifinder/Spotifinder/SpotifinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifinderApp.swift
+//  Spotifinder
+//
+//  Created by Martín Lago on 29/9/22.
+//
+
+import SwiftUI
+
+@main
+struct SpotifinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
